2845838f047be956e717f2544103c73ab561f0b6
Vendela Larsson <vendelalarsson@Vendelas-MacBook-Pro.local>
2017-10-16 12:38:58 -0400

    Lay out and add visuals for beginning of blog setup.
    Adding visual elements as I add sections. This commit includes
    background elements, the shrinking header, and the start of getting
    the correct content into the correct places. Working on setting up
    a grid and CSS organization that will scale well and make it easy
    to edit/adjust elements as I go along.

--- Gemfile
+++ Gemfile
@@ -1,8 +1,29 @@
+# If you do not have OpenSSL installed, change
+# the following line to use 'http://'
 source 'https://rubygems.org'
 
-gem 'middleman', '~> 4.2'
-gem 'middleman-autoprefixer', '~> 2.7'
+# For faster file watcher updates on Windows:
+gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw]
+
+# Windows does not come with time zone data
 gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
-gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
-gem 'middleman-livereload', '~> 3.4.3'
+
+# Include Bourbon
+gem 'bourbon'
+gem 'bitters'
 gem 'neat'
+
+# Middleman Gems
+gem "middleman", "~> 4.1"
+gem "middleman-blog"
+
+gem 'redcarpet', '~> 3.3', '>= 3.3.3'
+
+# For feed.xml.builder
+gem "builder", "~> 3.0"
+
+# Reload the page when changes are made
+gem 'middleman-livereload'
+
+# Article Summaries
+gem 'nokogiri'
--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,37 @@
-// This is where it all goes :)
+function shrinkHeader() {
+    window.addEventListener('scroll', function(e){
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            nav = document.getElementsByClassName('nav')[0];
+            logo = document.getElementsByClassName('logo')[0];
+            if (distanceY < 15) {
+                var navPaddingOffsetCalc = 30 - distanceY;
+            }
+            else {
+                var navPaddingOffsetCalc = 15;
+            }
+            if (distanceY < 138) {
+                var navOffset = 225 - distanceY;
+                nav.setAttribute('style', 'height:' +
+                     navOffset + 'px; padding:' +
+                     navPaddingOffsetCalc + 'px 30px;');
+            }
+            else {
+                nav.setAttribute('style',
+                    'height: 87px; padding: 15px 30px;');
+            }
+            if (distanceY < 103) {
+                var logoOffset = 160 - distanceY;
+                logo.setAttribute('style', 'height:' +
+                     logoOffset + 'px;');
+            }
+            else {
+                logo.setAttribute('style', 'height: 57px;');
+            }
+
+    });
+}
+window.onload = shrinkHeader();
+
+
+// for every 10 pixels that i scroll (distanceY), make the paddingoffset 1px,
+// as long as the distance i scroll is under 150px
