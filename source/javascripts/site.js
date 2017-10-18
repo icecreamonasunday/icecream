24718fe62b0113d56f0885d16922ba17b478c95e
vendelal <vendelalinnea@mac.com>
2017-10-18 17:04:16 -0400

    Add ahkio heading font and style headings and links
    This PR adds 'Ahkio' as the heading font and adds specific CSS
    definitions for H1-H5 elements so that it matches the style of
    the mockups. It also adds some hover states for links as well
    as typographic and some other styling for the jumbo-feature
    section to also match the sketch mockup.

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,42 +1,19 @@
 function shrinkHeader() {
     window.addEventListener('scroll', function(e){
         var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-            nav = document.getElementsByClassName('nav')[0];
-            logo = document.getElementsByClassName('logo')[0];
-            if (distanceY < 15) {
-                var navPaddingOffsetCalc = 30 - distanceY;
+            blogTitle = document.getElementsByClassName('blog-title')[0];
+            blogTitleFirstLine = document.getElementsByClassName('blog-title-first-line')[0];
+            if (distanceY < 57) {
+                var titleSizeOffset = 87 - distanceY;
+                blogTitle.setAttribute('style', 'font-size:' +
+                     titleSizeOffset + 'px; line-height:' + titleSizeOffset + 'px;');
             }
             else {
-                var navPaddingOffsetCalc = 15;
+                blogTitle.setAttribute('style', 'font-size: 30px; line-height: 27px;');
             }
-            if (distanceY < 138) {
-                var navOffset = 225 - distanceY;
-                nav.setAttribute('style', 'height:' +
-                     navOffset + 'px; padding:' +
-                     navPaddingOffsetCalc + 'px 30px;');
-            }
-            else {
-                nav.setAttribute('style',
-                    'height: 87px; padding: 15px 30px;');
-            }
-            if (distanceY < 103) {
-                var logoOffset = 160 - distanceY;
-                logo.setAttribute('style', 'height:' +
-                     logoOffset + 'px;');
-            }
-            else {
-                logo.setAttribute('style', 'height: 57px;');
-            }
-
     });
 }
 window.onload = shrinkHeader();
-
-
-// for every 10 pixels that i scroll (distanceY), make the paddingoffset 1px,
-// as long as the distance i scroll is under 150px
-
-
 
 $(document).ready(function() {
     $(".fadeIn").each(function() {
